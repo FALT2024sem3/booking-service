06d14b2e0547a9e03b510a12c76dd359c4ae436a
PetrosiunArtem <petrosiun.av@phystech.edu>
2025-12-15 00:29:49 +0300

    add notification kafka

---
+++ project/internal/handler/handler.go
@@ -0,0 +1,49 @@
+package handler
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"encoding/json"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	"github.com/sirupsen/logrus"
+)
+
+type Handler struct{}
+
+func NewHandler() *Handler {
+	return &Handler{}
+}
+
+func (h *Handler) HandleMessage(message []byte, topic kafka.TopicPartition, cn int) error {
+	logrus.Infof("Consumer #%d, Message from kafka with offset %d '%s' on partition %d", cn, topic.Offset, string(message), topic.Partition)
+	data := map[string]interface{}{
+		"to_addr":  string(message),
+		"subject":  "Teen Titans GO",
+		"template": "hello_email",
+		"vars": map[string]string{
+			"Name": "User",
+		},
+	}
+
+	jsonData, _ := json.Marshal(data)
+
+	resp, err := http.Post(
+		"http://localhost:8080/html_email",
+		"application/json",
+		bytes.NewBuffer(jsonData),
+	)
+	if err != nil {
+		log.Println("asasas")
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, _ := io.ReadAll(resp.Body)
+	fmt.Printf("Response my: %s\n", body)
+	return nil
+}
